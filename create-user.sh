818b6ac749de2f7392ae9cff09e4ad2f6d9a2fa8
Nico Braun <57263007+Obama360@users.noreply.github.com>
2023-01-16 10:50:42 +0100

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -4,17 +4,26 @@
 # Created 2023-1
 # Nico Braun
 
-#read -p "CSV File location: " csvPath
 csvPath=$1
-seperatorNormal=";"
+seperator=";"
+seperatorGroups=","
 
 #get basedn from first line
 basedn=$(head -n 1 "$csvPath")
 basedn=$(printf "%s\n" "${basedn//'basedn='}")
 echo "$basedn"
 
-#rest of file
-while IFS="$seperatorNormal" read -r username name surname password groups; do
+#process users
+while IFS="$seperator" read -r username name surname password groups; do
+  #get groups and put them into an array
   groups=$(echo $groups | sed 's/\[//g' | sed 's/\]//g')
-  echo "Username=$username, Password=$password, Groups=$groups"
+  IFS="$seperatorGroups" read -r -a groups <<< "$groups"
+
+  #create user
+  echo "Username=$username, Password=$password, Groups=${groups[0]}"
+
+  #add user to groups
+  for group in "${groups[@]}"; do
+    echo "$group"
+  done
 done < <(tail -n +2 "$csvPath")
