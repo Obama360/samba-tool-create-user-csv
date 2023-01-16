5e0a9ee7aa1a2219290be16dd6122438d4b75bbd
Nico Braun <57263007+Obama360@users.noreply.github.com>
2023-01-16 11:09:50 +0100

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -16,14 +16,15 @@ echo "$basedn"
 #process users
 while IFS="$seperator" read -r username name surname password groups; do
   #get groups and put them into an array
-  groups=$(echo $groups | sed 's/\[//g' | sed 's/\]//g')
+  groups=$(echo $groups | sed 's/\[//g' | sed 's/\]//g') #change the [ & ] characters here if needed
   IFS="$seperatorGroups" read -r -a groups <<< "$groups"
 
   #create user
   echo "Username=$username, Password=$password, Groups=${groups[0]}"
+  samba-tool user add "$username" "$password" --base-dn="$basedn" --given-name="$name" --surname="$surname" --display-name="$name $surname" -samaccountname="$username" && echo "created user $username" || echo "failed to create user $username"
 
   #add user to groups
   for group in "${groups[@]}"; do
-    echo "$group"
+    samba-tool group addmembers $group $username && echo "added $username to group $group" || echo "failed to add $username to $group!"
   done
 done < <(tail -n +2 "$csvPath")
