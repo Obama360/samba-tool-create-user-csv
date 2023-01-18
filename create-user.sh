bdc506175c069935eeac2eb86fc7219b145524c2
Nico Braun <57263007+Obama360@users.noreply.github.com>
2023-01-18 10:49:30 +0100

    Update create-user.sh

--- create-user.sh
+++ create-user.sh
@@ -30,7 +30,7 @@ while IFS="$seperator" read -r username name surname password groups; do
   IFS="$seperatorGroups" read -r -a groups <<< "$groups"
 
   #create user
-  sudo samba-tool user create "$username" "$password" --given-name="$name" --surname="$username" --userou="$ouPath" && echo "created user $username" || echo "failed to create user $username"
+  sudo samba-tool user create "$username" "$password" --given-name="$name" --surname="$surname" --userou="$ouPath" && echo "created user $username" || echo "failed to create user $username"
 
   #add user to groups
   for group in "${groups[@]}"; do
